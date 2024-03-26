1ac1df264b1c0bd63c29cd6b824241d8a70b91fb
Jhalak <stdjhalak@gmail.com>
2024-03-26 23:00:05 +0600

    read books and wishlist books route done

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
-import { getStoredListedReadBooks } from "../../utilities/localStorage";
+import { getStoredListedReadBooks, getStoredListedWishlistBooks } from "../../utilities/localStorage";
 import DisplayList from "../DisplayList/DisplayList";
 
 
@@ -9,21 +9,41 @@ const ListedBooks = () => {
     const books=useLoaderData();
     const [listedReadBooks,setListedReadBooks]=useState([]);
     const [displayReadBooks,setDisplayReadBooks]=useState([]);
-    const handleJobs=filter=>{
-        if(filter==='rating'){
-            const rating=listedReadBooks.filter(book=>book.rating)
-            setDisplayReadBooks(rating);  
-        }
-        // else if(filter==='total'){
-        //     const remote=jobApplied.filter(job=>job.remote_or_onsite==='Remote');
-        //     setDisplayJobs(remote);
-        // }
-        // else if(filter==='year'){
-        //     const onsite=jobApplied.filter(job=>job.remote_or_onsite==='Onsite');
-        //     setDisplayJobs(onsite);
-        // }
-    }
+    const [listedWishListBooks,setListedWishListBooks]=useState([]);
+    const [displayWishListBooks,setDisplayWishListBooks]=useState([]);
+    // const handleJobs=filter=>{
+    //     if(filter==='rating'){
+    //         const rating=listedReadBooks.filter(book=>book.rating)
+    //         setDisplayReadBooks(rating);  
+    //     }
+    //     else if(filter==='total'){
+    //         const remote=jobApplied.filter(job=>job.remote_or_onsite==='Remote');
+    //         setDisplayJobs(remote);
+    //     }
+    //     else if(filter==='year'){
+    //         const onsite=jobApplied.filter(job=>job.remote_or_onsite==='Onsite');
+    //         setDisplayJobs(onsite);
+    //     }
+    // }
 
+    useEffect(()=>{
+        const storedWishListBookIds=getStoredListedWishlistBooks();
+        if(books.length>0){
+            const listedBooks=[];
+            for(const id of storedWishListBookIds){
+                const book=books.find(book=>book.bookId===id);
+                if(book){
+                    listedBooks.push(book);
+                }
+            }
+            setListedWishListBooks(listedBooks);
+            setDisplayWishListBooks(listedBooks);
+           
+              
+           
+           
+        }
+    },[books])
     useEffect(()=>{
         const storedReadBookIds=getStoredListedReadBooks();
         if(books.length>0){
@@ -44,21 +64,27 @@ const ListedBooks = () => {
     },[books])
     return (
         <div className="mx-60 my-20">
-            <h2>Applied : {listedReadBooks.length}</h2>
-            <div className="text-right mb-16">
-            <details className="dropdown mb-10">
-  <summary className="m-1 btn">open or close</summary>
-  <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-    <li onClick={()=>handleJobs('rating')}><a>Rating</a></li>
-    <li onClick={()=>handleJobs('total')}><a>Number of Pages</a></li>
-    <li onClick={()=>handleJobs('year')}><a>Publisher Year</a></li>
-    
-  </ul>
-</details>
-            </div>
-            {
+            <div role="tablist" className="tabs tabs-lifted">
+  
+  <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Read Books" checked />
+  <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+  {
                 displayReadBooks.map(book=><DisplayList key={book.bookId} book={book}></DisplayList>)
-            }
+  }
+
+  </div>
+
+  <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Wishlist Books" />
+  <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+  {
+                displayWishListBooks.map(book=><DisplayList key={book.bookId} book={book}></DisplayList>)
+  }
+  </div>
+</div>
+            
+            {/* {
+                displayReadBooks.map(book=><DisplayList key={book.bookId} book={book}></DisplayList>)
+            } */}
             
         </div>
     );
