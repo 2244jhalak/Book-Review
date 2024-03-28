9e010b01bf9d736213b2916e6f3a7cb09f344720
Jhalak <stdjhalak@gmail.com>
2024-03-28 19:47:21 +0600

    font roboto successfully added

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,17 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
+
 
 
 const Root = () => {
     return (
-        <div>
+        <div className="font-roboto">
             <div className="container mx-auto">
                 <Header></Header>
                 <Outlet></Outlet>
                 
             </div>
-            <Footer></Footer>
+            
             
         </div>
     );
